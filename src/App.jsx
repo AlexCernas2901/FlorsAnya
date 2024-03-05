5b06dd379d9f4470e09ac101d802caa0705b82a0
Ángel Alexis Cernas Hernández <acernas1@ucol.mx>
2024-03-05 17:23:05 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { Header } from './components/Header.jsx'
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <h1>FlorsAnya</h1>
+    </div>
+  )
+}
