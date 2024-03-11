ef1e281a00ab2094bff695fe7c9b968bfcd8ae9a
jorgeaguil <adolsilvar@gmail.com>
2024-03-11 14:23:32 -0600

    menu state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import '../styles/Header.css'
-import { SearchIcon, MenuIcon } from '../icons/HeaderIcons.jsx'
+import { SearchIcon, MenuIcon, CloseMenu } from '../icons/HeaderIcons.jsx'
+import { useState } from 'react'
 
 export function Header() {
+  const [menu, setMenu] = useState(true)
+
+  const haandleClick = () => {
+    setMenu(!menu)
+  }
+
   return (
     <header className='header'>
       <div className='header-div-container'>
@@ -16,8 +23,12 @@ export function Header() {
               </button>
             </li>
             <li>
-              <button className='nav-button' aria-label='Menu'>
-                <MenuIcon />
+              <button
+                className='nav-button'
+                aria-label='Menu'
+                onClick={haandleClick}
+              >
+                {menu ? <MenuIcon /> : <CloseMenu />}
               </button>
             </li>
           </ul>
